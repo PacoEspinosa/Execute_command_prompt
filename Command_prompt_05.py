b90d414c780c254cdc9c0cfd4153234988caee1e
PacoEspinosa <paco.espinosamx@gmail.com>
2019-10-15 08:47:17 -0500

    List_files_path
    obtaining a list of files to proceess them

---
+++ Command_prompt_05.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 07:54:15 2019
+
+@author: francisco
+"""
+
+import os
+
+mycmd = 'rm *.bat'
+
+response = os.system(mycmd)
---
+++ get_names_files_path.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 08:15:58 2019
+
+@author: francisco
+"""
+
+import os
+
+path = os.getcwd()
+#path.replace('/','//')
+files = []
+
+for r, d, f in os.walk(path):
+    for file in f:
+        files.append(file)
+
+for f in files:
+    print(f)
